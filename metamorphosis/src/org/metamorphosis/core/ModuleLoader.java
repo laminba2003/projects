85a80cfd4bdb33d1d02cceb501cccdaffc2d9798
laminba2003 <laminba2003@yahoo.fr>
2016-10-31 10:26:40 +0000

    module loader

--- metamorphosis/src/org/metamorphosis/core/ModuleLoader.java
+++ metamorphosis/src/org/metamorphosis/core/ModuleLoader.java
@@ -7,17 +7,17 @@ public class ModuleLoader {
 
 	public Module loadModule(File file) throws Exception {
 		Digester digester = new Digester();
-        digester.setValidating( false );
-        digester.addObjectCreate("module", Module.class );
-        digester.addBeanPropertySetter("module/name");
-        digester.addBeanPropertySetter("module/url");
-        digester.addBeanPropertySetter("module/author");
-        digester.addBeanPropertySetter("module/authorEmail");
-        digester.addBeanPropertySetter("module/authorUrl");
-        digester.addBeanPropertySetter("module/creationDate");
-        digester.addBeanPropertySetter("module/copyright");
-        digester.addBeanPropertySetter("module/license");
-        digester.addBeanPropertySetter("module/version");
+		digester.setValidating( false );
+		digester.addObjectCreate("module", Module.class );
+		digester.addBeanPropertySetter("module/name");
+		digester.addBeanPropertySetter("module/url");
+		digester.addBeanPropertySetter("module/author");
+		digester.addBeanPropertySetter("module/authorEmail");
+		digester.addBeanPropertySetter("module/authorUrl");
+		digester.addBeanPropertySetter("module/creationDate");
+		digester.addBeanPropertySetter("module/copyright");
+		digester.addBeanPropertySetter("module/license");
+		digester.addBeanPropertySetter("module/version");
 		return (Module) digester.parse(file);
 	}
 }
