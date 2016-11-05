ddf611b64fbe70c12767a5e0c926eea6e08190f6
laminba2003 <laminba2003@yahoo.fr>
2016-11-05 14:34:55 +0000

    improving the core system.

--- metamorphosis/src/org/metamorphosis/core/ModuleInterceptor.java
+++ metamorphosis/src/org/metamorphosis/core/ModuleInterceptor.java
@@ -14,32 +14,37 @@ import com.opensymphony.xwork2.interceptor.AbstractInterceptor;
 @SuppressWarnings("serial")
 public class ModuleInterceptor extends AbstractInterceptor {
 
-    @SuppressWarnings("rawtypes")
+	@SuppressWarnings("rawtypes")
 	@Override
-    public String intercept(ActionInvocation invocation) throws Exception {
-        HttpServletRequest request = ServletActionContext.getRequest();
-        String uri = request.getRequestURI();
-        String url = uri.substring(request.getContextPath().length()+1,uri.length());
-        String action = url;
-        url = url.indexOf("/")!=-1 ? url.substring(0,url.indexOf("/")) : url;
-        Map application = (Map) ActionContext.getContext().get("application");
-		ModuleManager moduleManager = (ModuleManager) application.get("moduleManager");
-		request.setAttribute("modules",moduleManager.getVisibleModules());
-		Module module = moduleManager.getModuleByUrl(url); 
-		if(module!=null) {
-			request.setAttribute("module",module);
-			request.setAttribute("title",action);
-			request.setAttribute("js","modules/"+module.getId()+"/js");
-			request.setAttribute("css","modules/"+module.getId()+"/css");
-			request.setAttribute("images","modules/"+module.getId()+"/images");
-			List<MenuItem> items = new ArrayList<MenuItem>();
-			if(module.getMenu()!=null) {
-			    for(MenuItem item : module.getMenu().getMenuItems()) {
-					if(item.isVisible())items.add(item);
+	public String intercept(ActionInvocation invocation)  {
+		try {
+			HttpServletRequest request = ServletActionContext.getRequest();
+			String uri = request.getRequestURI();
+			String url = uri.substring(request.getContextPath().length()+1,uri.length());
+			String action = url;
+			url = url.indexOf("/")!=-1 ? url.substring(0,url.indexOf("/")) : url;
+			Map application = (Map) ActionContext.getContext().get("application");
+			ModuleManager moduleManager = (ModuleManager) application.get("moduleManager");
+			request.setAttribute("modules",moduleManager.getVisibleModules());
+			Module module = moduleManager.getModuleByUrl(url); 
+			if(module!=null) {
+				request.setAttribute("module",module);
+				request.setAttribute("title",action);
+				request.setAttribute("js","modules/"+module.getId()+"/js");
+				request.setAttribute("css","modules/"+module.getId()+"/css");
+				request.setAttribute("images","modules/"+module.getId()+"/images");
+				List<MenuItem> items = new ArrayList<MenuItem>();
+				if(module.getMenu()!=null) {
+					for(MenuItem item : module.getMenu().getMenuItems()) {
+						if(item.isVisible())items.add(item);
+					}
 				}
-		    }
-			request.setAttribute("menu",new Menu(items));
+				request.setAttribute("menu",new Menu(items));
+			}
+			return invocation.invoke();
+		}catch(Exception e) {
+
 		}
-        return invocation.invoke();
-    }
+		return "error";
+	}
 }
