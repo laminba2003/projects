8ce6bc1f40b4f4211c723521a309c1ed234b4d2f
laminba2003 <laminba2003@yahoo.fr>
2016-11-17 13:54:25 +0000

    improving the core system.

--- metamorphosis/src/org/metamorphosis/core/Module.java
+++ metamorphosis/src/org/metamorphosis/core/Module.java
@@ -7,77 +7,96 @@ public class Module extends Extension {
 
 	private int order;
 	private String url;
-    private boolean main;
-    private boolean visible = true;
-    private boolean administrable = true;
-    private String home="index.jsp";
-    private String template;
-    private Menu menu;
-    private List<Action> actions = new ArrayList<Action>();
-    
+	private boolean main;
+	private boolean visible = true;
+	private boolean administrable = true;
+	private String home = "index.jsp";
+	private String template;
+	private Menu menu;
+	private List<Action> actions = new ArrayList<Action>();
+
 	public int getOrder() {
 		return order;
 	}
+
 	public void setOrder(int order) {
 		this.order = order;
 	}
+
 	public String getUrl() {
 		return url;
 	}
+
 	public void setUrl(String url) {
 		this.url = url;
 	}
+
 	public boolean isMain() {
 		return main;
 	}
+
 	public void setMain(boolean main) {
 		this.main = main;
 	}
+
 	public boolean isVisible() {
 		return visible;
 	}
+
 	public void setVisible(boolean visible) {
 		this.visible = visible;
 	}
+
 	public boolean isAdministrable() {
 		return administrable;
 	}
+
 	public void setAdministrable(boolean administrable) {
 		this.administrable = administrable;
 	}
+
 	public String getHome() {
 		return home;
 	}
+
 	public void setHome(String home) {
 		this.home = home;
 	}
+
 	public String getTemplate() {
 		return template;
 	}
+
 	public void setTemplate(String template) {
 		this.template = template;
 	}
+
 	public Menu getMenu() {
 		return menu;
 	}
+
 	public void setMenu(Menu menu) {
 		this.menu = menu;
 	}
+
 	public List<Action> getActions() {
 		return actions;
 	}
+
 	public void setActions(List<Action> actions) {
 		this.actions = actions;
 	}
+
 	public void addAction(Action action) {
 		actions.add(action);
 	}
+
 	public String getIcon() {
-		return "modules/"+getId()+"/images/icon-16.png";
+		return "modules/" + getId() + "/images/icon-16.png";
 	}
-	
+
 	public String getPath() {
-		return "modules/"+getFolder().getName();
+		return "modules/" + getFolder().getName();
 	}
-	
+
 }
