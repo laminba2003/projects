20e1a17472633d99ac7f6bbec151167e8805ab9f
laminba2003 <laminba2003@yahoo.fr>
2016-11-22 15:59:32 +0000

    improving the core system.

--- builder/WebContent/modules/users/js/preferences.js
+++ builder/WebContent/modules/users/js/preferences.js
@@ -1,10 +1,10 @@
 app.ready(function(){
-	$(".template > a").click(function(){
-		 $(".templateDetails").hide();
-	    $(".templateDetails",$(this).parent()).fadeIn(1000);
+	$(".extension > a").click(function(){
+		 $(".extensionDetails").hide();
+	    $(".extensionDetails",$(this).parent()).fadeIn(1000);
 		return false;
 	});
 	$("body").click(function(){
-	    $(".templateDetails").fadeOut(1000);
+	    $(".extensionDetails").fadeOut(1000);
 	});
 });
