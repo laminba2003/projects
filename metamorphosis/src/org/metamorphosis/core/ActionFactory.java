460d11f82d106569f2a6db5b7a2206866815682f
laminba2003 <laminba2003@yahoo.fr>
2016-11-24 10:12:43 +0000

    improving the core system.

--- metamorphosis/src/org/metamorphosis/core/ActionFactory.java
+++ metamorphosis/src/org/metamorphosis/core/ActionFactory.java
@@ -14,25 +14,25 @@ public class ActionFactory extends DefaultActionFactory {
 
 	@SuppressWarnings("rawtypes")
 	@Override
-	public Object buildAction(String actionName, String namespace, ActionConfig config, Map<String, Object> extraContext) throws Exception {
+	public Object buildAction(String actionName, String namespace, ActionConfig config,Map<String, Object> extraContext) throws Exception {
 		Map application = (Map) ActionContext.getContext().get("application");
 		ModuleManager moduleManager = (ModuleManager) application.get("moduleManager");
 		Module module = moduleManager.getCurrentModule(ServletActionContext.getRequest());
-		if(module!=null) {
+		if (module != null) {
 			Action action = module.getAction(actionName);
-			if(action!=null && action.getScript()!=null) {
-				File script = new File(module.getFolder()+"/scripts/"+action.getScript());
-				if(script.exists()) {
+			if (action != null && action.getScript() != null) {
+				File script = new File(module.getFolder() + "/scripts/" + action.getScript());
+				if (script.exists()) {
 					String name = script.getName();
-					String extension = name.substring(name.indexOf(".")+1);
+					String extension = name.substring(name.indexOf(".") + 1);
 					ScriptEngine engine = new ScriptEngineManager().getEngineByExtension(extension);
-				    Object object = engine.eval(new FileReader(script));
-				    return object!=null ? object : engine.get("action");
+					Object object = engine.eval(new FileReader(script));
+					return object != null ? object : engine.get("action");
 				}
 			}
-			
+
 		}
-		return config.getClassName()!=null ? Class.forName(config.getClassName()).newInstance() : null;
+		return config.getClassName() != null ? Class.forName(config.getClassName()).newInstance() : null;
 	}
 
 }
