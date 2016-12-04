c84ad92065cb15529aafd0139eb7703d46b66ba0
laminba2003 <laminba2003@yahoo.fr>
2016-12-04 20:54:52 +0000

    improving the core system.

--- metamorphosis/src/org/metamorphosis/core/TilesContainerFactory.java
+++ metamorphosis/src/org/metamorphosis/core/TilesContainerFactory.java
@@ -6,14 +6,14 @@ import org.apache.tiles.mgmt.MutableTilesContainer;
 
 public class TilesContainerFactory extends org.apache.tiles.factory.TilesContainerFactory {
 
-	 public TilesContainer createContainer(Object context) throws TilesException {
+	public TilesContainer createContainer(Object context) throws TilesException {
 		return createMutableTilesContainer(context);
-	 }
-	 
-	 public MutableTilesContainer createMutableTilesContainer(Object context) throws TilesException {
-        CachingTilesContainer container = new CachingTilesContainer();
-        initializeContainer(context, container);
-        return container;
 	}
-	 
+
+	public MutableTilesContainer createMutableTilesContainer(Object context) throws TilesException {
+		CachingTilesContainer container = new CachingTilesContainer();
+		initializeContainer(context, container);
+		return container;
+	}
+
 }
