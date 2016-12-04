cd2f74595fe3f0d518c0f412ab84bcf89a3a6dc2
laminba2003 <laminba2003@yahoo.fr>
2016-12-04 21:05:19 +0000

    improving the core system.

--- metamorphosis/src/org/metamorphosis/core/CachingTilesContainer.java
+++ metamorphosis/src/org/metamorphosis/core/CachingTilesContainer.java
@@ -1,24 +1,23 @@
+
 package org.metamorphosis.core;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import org.apache.tiles.Definition;
 import org.apache.tiles.context.TilesRequestContext;
 import org.apache.tiles.definition.DefinitionsFactoryException;
 
 public class CachingTilesContainer extends org.apache.tiles.impl.mgmt.CachingTilesContainer {
 
-	 private Map<String,Definition> definitions = new HashMap<String,Definition>();
-	 
-	 public void register(Definition definition) throws DefinitionsFactoryException {
-	       definitions.put(definition.getName(), definition);
-	  }
-	 
-	 
-	  public Definition getDefinition(String definitionName,TilesRequestContext context) throws DefinitionsFactoryException {
-		    Definition definition = definitions.get(definitionName);
-	        return definition !=null ? definition : super.getDefinition(definitionName, context);
-	   }
+	private Map<String, Definition> definitions = new HashMap<String, Definition>();
+
+	public void register(Definition definition) throws DefinitionsFactoryException {
+		definitions.put(definition.getName(), definition);
+	}
+
+	public Definition getDefinition(String definitionName, TilesRequestContext context) throws DefinitionsFactoryException {
+		Definition definition = definitions.get(definitionName);
+		return definition != null ? definition : super.getDefinition(definitionName, context);
+	}
 
 }
