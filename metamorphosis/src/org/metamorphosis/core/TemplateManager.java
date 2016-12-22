7e842e95b97d57e9b1de989647f0bb3d4795a6b5
laminba2003 <laminba2003@yahoo.fr>
2016-12-22 22:28:09 +0000

    improving the core system.

--- metamorphosis/src/org/metamorphosis/core/TemplateManager.java
+++ metamorphosis/src/org/metamorphosis/core/TemplateManager.java
@@ -12,36 +12,36 @@ public class TemplateManager {
 	private List<Template> templates = new ArrayList<Template>();
 	private Logger logger = Logger.getLogger(TemplateManager.class.getName());
 	private static TemplateManager instance;
-    private static final String TEMPLATE_METADATA = "template.xml";
-    
+	private static final String TEMPLATE_METADATA = "template.xml";
+
 	public TemplateManager() {
 		instance = this;
 	}
 
 	public void loadTemplates(final File root) {
 		File[] files = root.listFiles();
-		if(files != null) {
-		  for(File folder : root.listFiles()) {
-			if(folder.isDirectory()) {
-			  loadTemplate(folder);
+		if (files != null) {
+			for (File folder : root.listFiles()) {
+				if (folder.isDirectory()) {
+					loadTemplate(folder);
+				}
 			}
-		  }
 		}
 		monitorRoot(root);
 	}
 
 	public Template loadTemplate(File folder) {
-		File metadata = new File(folder+"/"+TEMPLATE_METADATA);
-		if(metadata.exists()) {
-		  try {
-			 final Template template = parse(metadata);
-			 template.setFolder(folder);
-			 addTemplate(template);
-			 monitorTemplate(template);
-			 return template;
-		   } catch (Exception e) {
-			 e.printStackTrace();
-		   }
+		File metadata = new File(folder + "/" + TEMPLATE_METADATA);
+		if (metadata.exists()) {
+			try {
+				final Template template = parse(metadata);
+				template.setFolder(folder);
+				addTemplate(template);
+				monitorTemplate(template);
+				return template;
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
 		}
 		return null;
 	}
@@ -53,8 +53,8 @@ public class TemplateManager {
 		digester.addBeanPropertySetter("template/name");
 		digester.addBeanPropertySetter("template/type");
 		digester.addBeanPropertySetter("template/selected");
-		digester.addBeanPropertySetter("template/login","loginPage");
-		digester.addBeanPropertySetter("template/registration","registrationPage");
+		digester.addBeanPropertySetter("template/login", "loginPage");
+		digester.addBeanPropertySetter("template/registration", "registrationPage");
 		digester.addBeanPropertySetter("template/author");
 		digester.addBeanPropertySetter("template/authorEmail");
 		digester.addBeanPropertySetter("template/authorUrl");
@@ -65,53 +65,53 @@ public class TemplateManager {
 		digester.addBeanPropertySetter("template/version");
 		return (Template) digester.parse(metadata);
 	}
-	
+
 	private void monitorTemplate(final Template template) {
 		FileMonitor monitor = new FileMonitor(template.getFolder());
 		monitor.addListener(new FileListener() {
-			
+
 			@Override
 			public void onCreated(String file) {
-				if(file.equals(TEMPLATE_METADATA)) {
+				if (file.equals(TEMPLATE_METADATA)) {
 					updateTemplate(template);
-				}			
+				}
 			}
-			
+
 			@Override
 			public void onDeleted(String file) {
-				
+
 			}
 
 		});
 		monitor.watch();
 	}
-	
+
 	private void monitorRoot(final File root) {
 		FileMonitor monitor = new FileMonitor(root);
-		monitor.addListener(new FileListener(){
+		monitor.addListener(new FileListener() {
 
 			@Override
 			public void onCreated(String file) {
-				File folder = new File(root+"/"+file);
-				if(folder.isDirectory()) {
+				File folder = new File(root + "/" + file);
+				if (folder.isDirectory()) {
 					logger.log(Level.INFO, "adding template  : " + folder.getName());
 					final Template template = new Template();
 					template.setFolder(folder);
 					addTemplate(template);
 					monitorTemplate(template);
 				}
-				
+
 			}
 
 			@Override
 			public void onDeleted(String file) {
 				Template template = getTemplate(file);
-				if(template!=null) {
+				if (template != null) {
 					logger.log(Level.INFO, "removing template  : " + template.getName());
 					removeTemplate(template);
 				}
 			}
-			
+
 		});
 		monitor.watch();
 	}
@@ -121,51 +121,58 @@ public class TemplateManager {
 			logger.log(Level.INFO, "updating template  : " + template.getId());
 			int index = template.getIndex();
 			File folder = template.getFolder();
-			template = parse(new File(folder+"/"+TEMPLATE_METADATA));
+			template = parse(new File(folder + "/" + TEMPLATE_METADATA));
 			template.setFolder(folder);
 			template.setIndex(index);
 			templates.set(index, template);
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
-	
+
 	public void addTemplate(Template template) {
 		template.setIndex(templates.size());
 		templates.add(template);
 	}
-	
+
 	public void removeTemplate(Template template) {
 		templates.remove(template.getIndex());
 	}
 
 	public Template getTemplate(String id) {
-		for(Template template : templates) {
-			if(template.getId().equals(id)) return template;
+		for (Template template : templates) {
+			if (template.getId().equals(id))
+				return template;
 		}
 		return null;
 	}
 
 	public Template getBackendTemplate(String id) {
 		Template template = getTemplate(id);
-		if(template != null && template.isBackend()) return template;
-		for(Template current : templates) {
-			if(current.isSelected() && current.isBackend()) return current;
+		if (template != null && template.isBackend())
+			return template;
+		for (Template current : templates) {
+			if (current.isSelected() && current.isBackend())
+				return current;
 		}
-		for(Template current : templates) {
-			if(current.isBackend()) return current;
+		for (Template current : templates) {
+			if (current.isBackend())
+				return current;
 		}
 		return null;
 	}
 
 	public Template getFrontendTemplate(String id) {
 		Template template = getTemplate(id);
-		if(template != null && template.isFrontend()) return template;
-		for(Template current : templates) {
-			if(current.isSelected() && current.isFrontend()) return current;
+		if (template != null && template.isFrontend())
+			return template;
+		for (Template current : templates) {
+			if (current.isSelected() && current.isFrontend())
+				return current;
 		}
-		for(Template current : templates) {
-			if(current.isFrontend()) return current;
+		for (Template current : templates) {
+			if (current.isFrontend())
+				return current;
 		}
 		return null;
 	}
