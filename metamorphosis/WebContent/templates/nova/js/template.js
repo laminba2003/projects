483686be38c7a1ade422aa7afeef7102fb732109
laminba2003 <laminba2003@yahoo.fr>
2017-01-12 12:20:08 +0000

    improving the core system.

--- metamorphosis/WebContent/templates/nova/js/template.js
+++ metamorphosis/WebContent/templates/nova/js/template.js
@@ -7,6 +7,7 @@ page.form.reset = () => {
 	const wizard = $('#wizard').smartWizard('goToStep',1);
 	const length = $("> div",wizard).length;
 	for(var i=2;i<=length;i++) wizard.smartWizard('disableStep',i);
+	setTimeout(() => $("input:eq(0)",$(".stepContainer div:eq(0)")).focus(), 100);
 };
 
 const onFinish = () => {
@@ -15,11 +16,15 @@ const onFinish = () => {
 	return true;
 };
 
+const onShowStep = (step,context) => {
+	$("input:eq(0)",$(".stepContainer div").eq(context.toStep-1)).focus();
+	return true;
+};
 
 page.form.init = () => {
 	$('#form').css("display","block").addClass("mfp-hide");
 	head.load("templates/nova/js/jquery.smartWizard.js","templates/nova/js/jquery.magnific-popup.min.js", () => {
-		$('#wizard').addClass("swMain").smartWizard({noForwardJumping:true,onLeaveStep:()=>true,onFinish:onFinish});
+		$('#wizard').addClass("swMain").smartWizard({noForwardJumping:true,onShowStep:onShowStep,onFinish:onFinish});
 		$('#create').magnificPopup({type:'inline'}).click(() => page.form.reset());
 	});
 };
